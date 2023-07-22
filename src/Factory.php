90ea0004b1612abea62c59052e16f0da45597dfc
Björn Büttner <Idrinth@users.noreply.github.com>
2023-07-22 12:43:50 +0200

    Update Factory.php

--- src/Factory.php
+++ src/Factory.php
@@ -8,10 +8,10 @@ class Factory
 {
     public static function start(bool $isWriteable = false): void
     {
-        $duration = (int) ($_ENV['SESSION_DURATION'] ?? 7200);
-        $hasMemcached = extension_loaded('memcached') && ($_ENV['ENABLE_MEMCACHED'] ?? 'false') === 'true';
+        $duration = (int) ($_ENV['SESSION_DURATION'] ?: 7200);
+        $hasMemcached = extension_loaded('memcached') && ($_ENV['ENABLE_MEMCACHED'] ?: 'false') === 'true';
         session_set_save_handler(
-            $hasMemcached ? new Memcache($duration) : new File($duration, $_ENV['SESSION_PATH'] ?? sys_get_temp_dir()),
+            $hasMemcached ? new Memcache($duration) : new File($duration, $_ENV['SESSION_PATH'] ?: sys_get_temp_dir()),
             true
         );
         session_set_cookie_params([
